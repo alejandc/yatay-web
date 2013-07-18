0be6cdea670dd60acbe98ac72b81ca234d4dbae3
LarA <lara@lara-Lenovo-G460.(none)>
2013-07-18 11:08:28 -0300

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YatayWeb::Application.config.secret_key_base = '0c37a18e702d6179497ae96bb06b1bf04eee79378bf7c24c680a0d1cd76799223a95cccb8a74a9e4d6beffd568d4003e4c7ff633215939a41b6a04fa6dac244e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YatayWeb::Application.config.session_store :cookie_store, key: '_yatay-web_session'
