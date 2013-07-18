58a2dc2370ac5e982e50b37272827869a9f87c1f
LarA <lara@lara-Lenovo-G460.(none)>
2013-07-18 16:34:58 -0300

    receipt model, controller, migration

---
+++ app/models/receipt.rb
@@ -0,0 +1,16 @@
+class Receipt < ActiveRecord::Base
+
+Titular licencia de taxi:
+Documento Tipo:                                         Nro.:
+Domicilio legal:                                            
+Licencia Taxi Nro.:                                      Marca de auto:
+Marca de reloj:                                               Modelo año:
+Nro. de reloj:                                               Dominio Nro.:
+Nro. de Precinto:                                           Nro. de Motor:
+                                                                     Nro. de Chasis:
+
+Trabajos Realizados / Observaciones:
+
+Pulsos por Kilómetros:
+	
+end
