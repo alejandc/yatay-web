473d67d42b3afa81a742e95c78b50eedede82bf0
Alejandro Claveir <alejandro.claveir@gmail.com>
2013-07-19 00:26:26 -0300

    Se agrego el modelo user y la configuracion basi para la autenticacion...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 YatayWeb::Application.routes.draw do
+  devise_for :users do 
+    get "/", :to => "devise/sessions#new"
+    get "/users/sign_up", :to => "devise/sessions#new"
+  end
+  
+  resources :admin, :only => [:index]
+  namespace :admin do
+    resources :users, :except => [:show]
+  end
   resources :receipts
- 
-  root to: "receipts#index"
+  
+  root :to => "devise/sessions#new"
 end
