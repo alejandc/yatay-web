75fbfcb2d1ab428b0fd496ac72d78d52fa46f565
Alejandro Claveir <alejandro.claveir@gmail.com>
2013-07-21 23:05:51 -0300

    Actualizacion del inicio de la secuencia para el numero del certificado tecnico...

--- db/migrate/20130721191628_create_sequence_for_technical_certificate_number.rb
+++ db/migrate/20130721191628_create_sequence_for_technical_certificate_number.rb
@@ -1,5 +1,5 @@
 class CreateSequenceForTechnicalCertificateNumber < ActiveRecord::Migration
   def up
-    execute 'CREATE SEQUENCE technical_certificate_no START 1;'
+    execute 'CREATE SEQUENCE technical_certificate_no START 1600;'
   end
 end
