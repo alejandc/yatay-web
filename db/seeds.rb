a87627d342485e1ace055ff078b2ec0c86950e89
Alejandro Claveir <alejandro.claveir@gmail.com>
2013-07-21 23:08:59 -0300

    Se agrego usuario admin para que quede por default...

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,9 @@ user1.add_role :admin
 user2 = User.create(:username => "lara", :email => 'monti.lara.i@gmail.com', :password => "admin1234", :password_confirmation => "admin1234")
 user2.add_role :admin
 
+user3 = User.create(:username => "admin", :email => 'admin@admin.com', :password => "admin1234", :password_confirmation => "admin1234")
+user3.add_role :admin
+
 30.times do
   Certificate.create!(titular:             "Pedro Lopez",
                       document_type:       "DNI",
